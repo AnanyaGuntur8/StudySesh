aebf6a27e2f7c5aaae9aaf14edcc514d95f087c8
AnanyaGuntur8 <149723548+AnanyaGuntur8@users.noreply.github.com>
2024-08-12 11:33:48 -0400

    Update postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { requireSignin } = require('../controllers/userController');
-const { createPostController } = require('../controllers/postController');
+const { createPostController, getAllPostsController } = require('../controllers/postController');
 const router = express.Router();//router object
 
 
 //create post and post
 router.post('/create-post', requireSignin, createPostController)
+router.get('/get-all-posts', getAllPostsController)
 
 module.exports = router;//export router 
 
