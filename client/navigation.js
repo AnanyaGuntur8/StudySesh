4e32585f20a512b53ace559a3fb901144c8dacfa
AnanyaGuntur8 <149723548+AnanyaGuntur8@users.noreply.github.com>
2024-08-12 11:34:08 -0400

    Update navigation.js

--- client/navigation.js
+++ client/navigation.js
@@ -2,11 +2,15 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { AuthContext, AuthProvider } from './context/authContext'
 import ScreenMenu from './components/Menus/ScreenMenu'
+import { PostProvider } from './context/postContext'
 
 const RootNavigation = () => {
   return (
     <AuthProvider>
-        <ScreenMenu/>
+      <PostProvider>
+      <ScreenMenu/>
+      </PostProvider>
+        
     </AuthProvider>
   )
 }
