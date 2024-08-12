da279dc1f9f55b022a6cab63efb6fbb4a41cfb69
AnanyaGuntur8 <149723548+AnanyaGuntur8@users.noreply.github.com>
2024-08-12 11:33:54 -0400

    Update Home.js

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,36 +1,85 @@
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native';
 import React, { useContext } from 'react';
 import { AuthContext } from '../context/authContext';
 import FooterMenu from '../components/Menus/FooterMenu';
+import { PostContext } from '../context/postContext';
 
 const Home = () => {
-  // global state
+  // Global state
   const [state] = useContext(AuthContext);
+  const [posts] = useContext(PostContext);
 
   return (
     <SafeAreaView style={styles.safearea}>
-      <View style={styles.container}>
-        <Text style={styles.text}>{JSON.stringify(state, null, 4)}</Text>
-        <FooterMenu />
-      </View>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <View style={styles.container}>
+          {posts.map((post, index) => (
+            <View key={index} style={styles.row}>
+              {index % 2 === 0 && (
+                <>
+                  <TouchableOpacity style={styles.postButton}>
+                    <Text style={styles.postText}>{post.title}</Text>
+                    <Text style={styles.username}>@{post.postedBy?.username}</Text>
+                  </TouchableOpacity>
+                  {posts[index + 1] && (
+                    <TouchableOpacity style={styles.postButton}>
+                      <Text style={styles.postText}>{posts[index + 1].title}</Text>
+                      <Text style={styles.username}>@{post.postedBy?.username}</Text>
+                    </TouchableOpacity>
+                  )}
+                </>
+              )}
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+      <FooterMenu />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   safearea: {
-    flex: 1, 
-    backgroundColor: '#050315', 
+    flex: 1,
+    backgroundColor: '#050315',
+  },
+  scrollView: {
+    flexGrow: 1,
+    justifyContent: 'space-between',
   },
   container: {
     flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     justifyContent: 'space-between',
     marginTop: 80,
     marginLeft: 10,
   },
-  text: {
-    color: 'white',
-    paddingRight:10,
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginBottom: 10,
+  },
+  postButton: {
+    backgroundColor: '#00CFFF',
+    padding: 10,
+    height: 140, 
+    width: '48%', 
+    justifyContent: 'center',
+    alignItems: 'left',
+    borderRadius: 10,
+  },
+  postText: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  username: {
+    color: 'black',
+    fontSize: 15,
+    textAlign: 'center',
   },
 });
 
