df6f4f0f2fc02a515ad29f8e4d6883fbf2e99f96
AnanyaGuntur8 <149723548+AnanyaGuntur8@users.noreply.github.com>
2024-08-13 02:21:12 -0400

    Update PostCard.js

--- client/screens/PostCard.js
+++ client/screens/PostCard.js
@@ -1,42 +1,163 @@
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const PostCard = ({ route }) => {
-  const { post } = route.params;  // will help you access the data points of the post
+  const { post } = route.params;  // receive post data from params
+  const color = post.color; // no need for default color
+  const navigation = useNavigation();
 
-//using the data recieved by the post in the return
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={[styles.safearea, { backgroundColor: color }]}>
       <ScrollView>
-      <Text style={styles.title}>{post.title}</Text>
-      <Text style={styles.username}>@{post.postedBy?.username}</Text>
-      <Text style={styles.content}>{post.description}</Text>
-      <Text style={styles.content}>{post.update}</Text>
-      
+        <View style={styles.container}>
+          <View style={styles.titleAndIcon}>
+            <Entypo name='chevron-with-circle-left' style={styles.iconStyle} 
+            onPress={() => navigation.goBack()} />
+            <Text style={styles.title}>{post.title}</Text>
+          </View>
+          {/* since the margin isnt working, add empty spacer */}
+          
+          <Text style={styles.contentDescription}>Description</Text>
+          <Text style={styles.description}>{post.description}</Text>
+          
+          {/* Putting in the user name and the Join Group button in the same row */}
+          <View style={styles.usernameAndJoinContainer}>
+            <TouchableOpacity>
+              <Text style={styles.username}>@{post.postedBy?.username}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.joinGroup}>
+              <Text style={styles.joinButtonText}>Join Group</Text>
+            </TouchableOpacity>
+          </View>
+
+          <Text style={styles.content}>Update</Text>
+          <Text style={styles.update}>{post.update}</Text>
+          
+          {/* Delivering the post details to the page and navigating it to home */}
+          {/* Making the button, make an API for the notes */}
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity>
+              <Text style={styles.notes}>Notes</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.notes}>Community</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </ScrollView>
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  safearea: {
     flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  container: {
     padding: 20,
   },
-  title: {
-    paddingTop:50,
-    fontSize: 24,
-    fontWeight: 'bold',
+  titleAndIcon: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginBottom:50,
   },
-  content: {
-    fontSize: 18,
-    marginTop: 10,
+  iconStyle: {
+    fontSize: 40,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    paddingLeft: 10,
+  },
+  usernameAndJoinContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between', 
+    marginTop: 30,
   },
   username: {
     fontSize: 16,
-    marginTop: 20,
-    fontStyle: 'italic',
+    fontWeight: 'bold',
+    flex: 1,
   },
+  spacer: {
+    height: 10, 
+  },
+  description: {
+    borderRadius: 30,
+    fontSize: 16,
+    paddingBottom: 10,
+    // marginTop: 5,
+  },
+  contentDescription: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    borderWidth: 2,
+    borderRadius: 15,
+    height: 40,
+    width: 150,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    marginVertical: 10, 
+  },
+  joinGroup: {
+    fontSize: 16,
+    height: 40,
+    borderWidth: 1,
+    width: 100, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    backgroundColor: 'black',
+    borderRadius: 30,
+    marginLeft: 10, 
+  },
+  joinButtonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  content: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    borderWidth: 2,
+    borderRadius: 15,
+    height: 40,
+    width: 100,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    marginVertical: 10,
+  },
+  update: {
+    fontSize: 16,
+    paddingBottom: 30,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 2,
+  },
+  notes: {
+    fontSize: 16,
+    borderWidth: 2,
+    borderRadius: 15,
+    height: 150,
+    width: 150,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    marginVertical: 10,
+    fontWeight: 'bold',
+  }
 });
 
 export default PostCard;
