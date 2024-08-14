0d3c7fb067d232338e47e173f43811b3675256c8
AnanyaGuntur8 <149723548+AnanyaGuntur8@users.noreply.github.com>
2024-08-14 17:00:54 -0400

    Update postContext.js

--- client/context/postContext.js
+++ client/context/postContext.js
@@ -1,36 +1,30 @@
-import React, {createContext,useState, useEffect} from 'react';
-import axios from  'axios'
-//context
-const PostContext = createContext()//empty context
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
-//making the post provider
-const PostProvider = ({children}) => {
-const[loading, setLoading] = useState(false)
-const[posts, setPosts] = useState([]) //initial post is an arrya of post
-//getting all the posts
-const getAllPosts =  async ()=>{
-    try{
-        const {data} = await axios.get('/post/get-all-posts')
-        setLoading(false)
-        setPosts(data?.posts)
+// Create Context
+export const PostContext = createContext();
 
-    }
-    catch(error){
-        console.log(error),
-        setLoading(false)
-    }
-};
-//initial posts
-useEffect(()=>{
-    getAllPosts()
-},[])
-return(
+// Create Provider
+export const PostProvider = ({ children }) => {
+  const [posts, setPosts] = useState([]); // make it into an array
+  
+
+  useEffect(() => {
+    const getAllPosts = async () => {
+      try {
+        const { data } = await axios.get('/post/get-all-posts');
+        setPosts(data?.posts || []); 
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getAllPosts();
+  }, []);
+
+  return (
     <PostContext.Provider value={[posts, setPosts]}>
-        {children}
+      {children}
     </PostContext.Provider>
-)
-}
-//global state
-
-//exporting the module
-export {PostContext,PostProvider}
+  );
+};
