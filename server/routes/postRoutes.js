94e73cd7e1d30ec59fb5f6b5a7b9aa3dc415b186
AnanyaGuntur8 <149723548+AnanyaGuntur8@users.noreply.github.com>
2024-08-16 21:26:54 -0400

    Update postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { requireSignin } = require('../controllers/userController');
-const { createPostController, getAllPostsController, getUserPostsController, deletePostController } = require('../controllers/postController');
+const { createPostController, getAllPostsController, getUserPostsController, deletePostController, updatePostController } = require('../controllers/postController');
 const router = express.Router();//router object
 
 
@@ -9,5 +9,6 @@ router.post('/create-post', requireSignin, createPostController)
 router.get('/get-all-posts', getAllPostsController)
 router.get('/get-user-posts', requireSignin, getUserPostsController )
 router.delete('/delete-post/:id', requireSignin, deletePostController)
+router.put('/update-post/:id', requireSignin, updatePostController )
 
 module.exports = router;//export router 
