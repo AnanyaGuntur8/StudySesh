cfae2524d13bf200d6ed1113b6ba7547ae406399
AnanyaGuntur8 <149723548+AnanyaGuntur8@users.noreply.github.com>
2024-08-24 23:52:46 -0400

    Update socket.js

--- client/components/utils/socket.js
+++ client/components/utils/socket.js
@@ -1,5 +1,12 @@
-import { io } from "socket.io-client";
+import io from 'socket.io-client';
 
-const socket = io.connect("http://localhost:8080");
+const socket = io('http://localhost:8080'); // Adjust the URL as necessary
 
+socket.on('connect', () => {
+    console.log('Connected to server');
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+});
 export default socket;
