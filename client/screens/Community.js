617c4157f7dc83d4b48261b7f450bb60e5293107
AnanyaGuntur8 <149723548+AnanyaGuntur8@users.noreply.github.com>
2024-08-25 18:56:06 -0400

    Update Community.js

--- client/screens/Community.js
+++ client/screens/Community.js
@@ -1,8 +1,10 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, FlatList, Alert, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, FlatList, Alert, StyleSheet, KeyboardAvoidingView , Platform} from 'react-native';
 import axios from 'axios';
 import { AuthContext } from '../context/authContext';
 import { getSocket } from '../components/utils/socket'; // Adjust the path to your socket file
+import Entypo from 'react-native-vector-icons/Entypo';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 
 const Community = ({ route }) => {
     const [loading, setLoading] = useState(true);
@@ -12,6 +14,7 @@ const Community = ({ route }) => {
     const { token, user } = state;
     const { username } = user;
     const { post } = route.params;
+    const navigation = useNavigation();
 
     const postColor = post.color || '#007bff';
     const postId = post._id;
@@ -90,61 +93,102 @@ const Community = ({ route }) => {
     };
 
     return (
-        <View style={[styles.container, { backgroundColor: postColor }]}>
-            <Text style={styles.title}>Community</Text>
-            <Text style={styles.subtitle}>{post.title}</Text>
-            {loading ? (
-                <Text>Loading messages...</Text>
-            ) : (
-                <FlatList
-                    data={messages}
-                    inverted
-                    keyExtractor={(item) => item.id ? item.id : Math.random().toString()}
-                    renderItem={({ item }) => (
-                        <View style={[styles.messageContainer, { backgroundColor: postColor }]}>
-                            <Text style={styles.message}>
-                                <Text style={styles.sender}>{item.sender}: </Text>
-                                {item.message}
-                            </Text>
-                        </View>
-                    )}
-                    contentContainerStyle={{ paddingBottom: 30 }}
-                />
-            )}
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Type your message..."
-                    value={newMessage}
-                    onChangeText={setNewMessage}
-                />
-                <TouchableOpacity style={styles.sendButton} onPress={handleSendMessage}>
-                    <Text style={styles.sendButtonText}>Send</Text>
-                </TouchableOpacity>
+        <KeyboardAvoidingView
+            style={[styles.container, { backgroundColor: postColor }]}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={1} 
+        >
+            <View style={[styles.container, { backgroundColor: postColor }]}>
+                <View style={[styles.borderContainer, { backgroundColor: postColor }]}>
+                    <View style={styles.titleAndIcon}>
+                    {/* add the back arrow */}
+                        <TouchableOpacity onPress={() => navigation.goBack()}>
+                        <Entypo name='chevron-with-circle-left' style={styles.iconStyle} />
+                        </TouchableOpacity>
+                    
+                    {/* add the back arrow */}
+                    <Text style={styles.title}>Community</Text>
+                    </View>
+                    <Text style={styles.subtitle}>{post.title}</Text>
+                
+                
+                </View>
+                {loading ? (
+                    <Text>Loading messages...</Text>
+                ) : (
+                    <FlatList
+                        data={messages}
+                        inverted
+                        keyExtractor={(item) => item.id ? item.id : Math.random().toString()}
+                        renderItem={({ item }) => (
+                            <View style={[styles.messageContainer, { backgroundColor: postColor }]}>
+                                <Text style={styles.message}>
+                                    <Text style={styles.sender}>{item.sender}: </Text>
+                                    {item.message}
+                                </Text>
+                            </View>
+                        )}
+                        contentContainerStyle={{ paddingBottom: 30 }}
+                    />
+                )}
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Type your message..."
+                        value={newMessage}
+                        onChangeText={setNewMessage}
+                    />
+                    <TouchableOpacity style={styles.sendButton} onPress={handleSendMessage}>
+                        <Text style={styles.sendButtonText}>Send</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-        </View>
+        </KeyboardAvoidingView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 20,
+        // padding: 2,
+        // paddingBottom:20,
     },
     title: {
-        paddingTop: 60,
-        fontSize: 24,
+        paddingTop: 20,
+        fontSize: 30,
         fontWeight: 'bold',
     },
     subtitle: {
-        paddingTop: 10,
-        fontSize: 18,
-        color: '#666',
+        paddingTop: 1,
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'black',
+        paddingBottom: 10,
+        paddingLeft: 70,
+    },
+    titleAndIcon: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        // marginBottom: 10,
+        paddingLeft: 20,
+        paddingTop:40,
+    },
+    iconStyle: {
+        paddingTop:20,
+        fontSize: 40,
+        marginRight: 10,
+    },
+    borderContainer: {
+        paddingBottom: 10,
+        borderBottomWidth: 2, // Border width
+        borderBottomColor: 'black', // Border color
+        width: '100%',
     },
     messageContainer: {
         marginVertical: 5,
+        marginHorizontal: 10,
         borderRadius: 10,
-        padding: 10,
+        padding: 8,
     },
     message: {
         color: 'black',
@@ -156,16 +200,21 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         marginTop: 10,
+        marginLeft: 15,
+        marginRight: 15,
+        paddingBottom: 20,
     },
     input: {
         flex: 1,
-        borderColor: '#ccc',
-        borderWidth: 1,
+        borderColor: 'black',
+        borderWidth: 2,
         borderRadius: 20,
         padding: 10,
         marginRight: 10,
+        color: 'black',
     },
     sendButton: {
+        backgroundColor: 'black',
         borderRadius: 20,
         padding: 10,
     },
