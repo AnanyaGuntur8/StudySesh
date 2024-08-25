75ea6b9c8d789bfb0346bd508ba07360c357dc32
AnanyaGuntur8 <149723548+AnanyaGuntur8@users.noreply.github.com>
2024-08-24 23:52:39 -0400

    Update chatRoutes.js

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -3,10 +3,12 @@ const { requireSignin } = require('../controllers/userController');
 const { postMessageController, getMessagesController } = require('../controllers/chatController');
 const router = express.Router();
 
+module.exports = (io) => {
+    // Route to get messages for a specific post
+    router.get('/:postId/messages', requireSignin, getMessagesController);
 
-router.get('/:postId/messages', requireSignin, getMessagesController);
-// Updated route to match the structure in your Community component
-router.post('/:postId/messages', requireSignin, postMessageController);
+    // Route to post a new message for a specific post
+    router.post('/:postId/messages', requireSignin, (req, res) => postMessageController(req, res, io));
 
-
-module.exports = router;
+    return router;
+};
